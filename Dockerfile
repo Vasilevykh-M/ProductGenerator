6e4a9ad40d29e7b9b5fc3f91443d1b3ed5b85005
Vasilevykh-M <vasilevyh00@gmail.com>
2024-09-26 00:49:52 +0500

    lfs

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f1003115623196c9ea3e7f51fb4905460213aee7caec26c5c5c1f2a06b43d4a
+size 724
---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72bfeeef44ecae018a7a0cce1566c6c08a62c1f5fe3c450f51ac0c4d2e19ec82
+size 341
